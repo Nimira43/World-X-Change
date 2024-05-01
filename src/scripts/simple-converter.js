e11b37139308a15719af63985b13c25c0c49647b
Nick Rabone <n.rabone72@aol.com>
2024-05-01 01:44:47 +0100

    Introducing ExchangeRate API to get data

--- src/scripts/simple-converter.js
+++ src/scripts/simple-converter.js
@@ -3,13 +3,27 @@ const convertBtn = document.getElementById('convert')
 convertBtn.onclick = () => convert()
 
 function convert() {
-    const amount = document.getElementById('amount').value
+    let amount = document.getElementById('amount').value
     const from = document.getElementById('from').value
     const to = document.getElementById('to').value
     const result = document.getElementById('result').value
-    if (amount === '') {
+    if (amount === '' || isNaN(amount)) {
         alert('Enter amount to convert: ')
+        return
     }
-    
+    amount = Number(amount)
+    console.log(typeof amount)
     console.log(amount, from, to, result)
+
+// https://www.exchangerate-api.com/
+
+    const url = `https://api.exchangerate-api.com/v4/latest/${from}`
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const rate = data.rates['VND']
+            console.log(rate)
+            console.log(data)
+    })
 }
